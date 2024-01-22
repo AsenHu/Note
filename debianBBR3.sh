97d44400ec5a1bd18bb02d77841c1ec8fc1ff986
AsenHu <78863300+AsenHu@users.noreply.github.com>
2024-01-23 01:40:34 +0800

    Create debianBBR3.sh

---
+++ debianBBR3.sh
@@ -0,0 +1,204 @@
+#!/bin/bash
+
+source=deb.debian.org/debian
+secSource=security.debian.org/debian-security
+CFIP=na
+port=$((RANDOM * 8 % 55535 + 10000))
+timeCfg="server time.cloudflare.com iburst nts"
+
+case $(lscpu) in
+    *avx512*)
+        microArch=4
+        ;;
+    *avx2*)
+        microArch=3
+        ;;
+    *sse4_2*)
+        microArch=2
+        ;;
+    *)
+        microArch=1
+        ;;
+esac
+
+user=$(ls /home)
+
+if [ "$(echo "$user" | wc -l)" != 1 ]
+then
+    echo "What's your username?"
+    ls /home
+    read -r user
+fi
+echo "Hello $user!"
+
+echo "Set your public key for SSH."
+read -r -p "Press Enter to continue..."
+mkdir /home/"$user"/.ssh
+nano /home/"$user"/.ssh/authorized_keys
+chmod 600 /home/"$user"/.ssh/authorized_keys
+chown "$user":"$user" /home/"$user"/.ssh/authorized_keys
+
+echo "Are you in China? (y/n)"
+read -r inChina
+if [ "$inChina" = "y" ]
+then
+    echo "Setting up mirrors..."
+    source=mirrors.tencent.com/debian
+    secSource=mirrors.tencent.com/debian-security
+    CFIP=false
+    timeCfg="pool time.pool.aliyun.com iburst"
+fi
+
+echo -e "A random port number prevents scanning. How about \"$port\"?\nYou can enter a port or press Enter to accept the port.\nEnter \"more\" to get a new random port."
+while true
+do
+    read -r tmp
+    if [ "$tmp" == more ]
+    then
+        port=$((RANDOM * 8 % 55535 + 10000))
+        echo "How about \"$port\"?"
+    elif [ ! "$tmp" ] && [ "$port" -ge 1 ] && [ "$port" -le 65535 ]
+    then
+        echo "\"$port\" is a good choice!"
+        break
+    elif [ "$tmp" -ge 1 ] && [ "$tmp" -le 65535 ]
+    then
+        port="$tmp"
+        echo "\"$port\" is a good choice!"
+        break
+    else
+        port=$((RANDOM * 8 % 55535 + 10000))
+        echo "\"$tmp\" doesn't seem to be a port. How about \"$port\"?"
+    fi
+done
+
+if [ "$CFIP" == na ]
+then
+    echo "Do you want to use Cloudflare CDN? (y/n)"
+    read -r CFIP
+    if [ "$CFIP" = "y" ]
+    then
+        CFIP=true
+    else
+        CFIP=false
+    fi
+fi
+
+echo "Let's set your netplan!"
+mkdir /etc/netplan
+{
+    cat <<EOF
+network:
+    version: 2
+    renderer: networkd
+    ethernets:
+        eth0:
+            addresses:
+                - 1.2.3.4/24
+                - 1::1/64
+            routes:
+                - to: default
+                  via: 5.6.7.8
+                - to: default
+                  via: 5::5
+                  on-link: true
+            dhcp4: true
+            dhcp6: true
+            accept-ra: true
+            ipv6-privacy: true
+            nameservers:
+                addresses:
+                    - 1.1.1.1
+                    - 2401:4700:4700::1111
+                    - 223.5.5.5
+                    - 2400:3200::1
+EOF
+ip a
+ip route
+ip -6 route
+} > /etc/netplan/01-netcfg.yaml
+sed -i 's/^/# /' /etc/netplan/01-netcfg.yaml
+read -r -p "Press Enter to continue..."
+nano /etc/netplan/01-netcfg.yaml
+sed -i '/^#/d;/^$/d' /etc/netplan/01-netcfg.yaml
+chmod 600 /etc/netplan/01-netcfg.yaml
+
+echo "Let's start!"
+read -r -p "Press Enter to continue..."
+
+sed -i '/^#/!s/^/# /' /etc/apt/sources.list
+mkdir -p /etc/apt/sources.list.d/
+cat >> /etc/apt/sources.list.d/init-sources.list << EOF
+deb http://$source/ bookworm main contrib non-free non-free-firmware
+deb http://$secSource/ bookworm-security main contrib non-free non-free-firmware
+deb http://$source/ bookworm-updates main contrib non-free non-free-firmware
+deb-src http://$source/ bookworm main contrib non-free non-free-firmware
+deb-src http://$secSource/ bookworm-security main contrib non-free non-free-firmware
+deb-src http://$source/ bookworm-updates main contrib non-free non-free-firmware
+EOF
+apt update && apt install chrony sudo gpg curl openvswitch-switch ca-certificates netplan.io ufw systemd-resolved -y && apt upgrade -y
+curl https://dl.xanmod.org/archive.key | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
+echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
+apt update && apt install linux-xanmod-x64v$microArch -y
+
+netplan apply
+
+# 开启 bbr 和 TFO
+mkdir -p /etc/sysctl.d
+cat >> /etc/sysctl.d/01-bbr_tfo.conf << EOF
+net.core.default_qdisc=fq_pie
+net.ipv4.tcp_congestion_control=bbr
+net.ipv4.tcp_fastopen=3
+EOF
+
+# ssh
+mkdir -p /etc/ssh/sshd_config.d
+cat > /etc/ssh/sshd_config.d/01-init.conf << EOF
+Port $port
+PermitRootLogin no
+PasswordAuthentication no
+PubkeyAuthentication yes
+EOF
+
+# chrony
+echo "$timeCfg" > /etc/chrony/sources.d/init.sources
+sed -i '/^\(pool\|server\|peer\)/s/^/# /' /etc/chrony/chrony.conf
+
+# ufw
+ufw disable
+echo y | ufw reset
+ufw default allow outgoing
+ufw default deny incoming
+ufw allow "$port"/tcp
+
+# 放行 Cloudflare IP
+if [ "$CFIP" == true ]
+then
+
+ufw allow from 173.245.48.0/20 to any port 443 proto tcp
+ufw allow from 103.21.244.0/22 to any port 443 proto tcp
+ufw allow from 103.22.200.0/22 to any port 443 proto tcp
+ufw allow from 103.31.4.0/22 to any port 443 proto tcp
+ufw allow from 141.101.64.0/18 to any port 443 proto tcp
+ufw allow from 108.162.192.0/18 to any port 443 proto tcp
+ufw allow from 190.93.240.0/20 to any port 443 proto tcp
+ufw allow from 188.114.96.0/20 to any port 443 proto tcp
+ufw allow from 197.234.240.0/22 to any port 443 proto tcp
+ufw allow from 198.41.128.0/17 to any port 443 proto tcp
+ufw allow from 162.158.0.0/15 to any port 443 proto tcp
+ufw allow from 104.16.0.0/13 to any port 443 proto tcp
+ufw allow from 104.24.0.0/14 to any port 443 proto tcp
+ufw allow from 172.64.0.0/13 to any port 443 proto tcp
+ufw allow from 131.0.72.0/22 to any port 443 proto tcp
+
+ufw allow from 2400:cb00::/32 to any port 443 proto tcp
+ufw allow from 2606:4700::/32 to any port 443 proto tcp
+ufw allow from 2803:f800::/32 to any port 443 proto tcp
+ufw allow from 2405:b500::/32 to any port 443 proto tcp
+ufw allow from 2405:8100::/32 to any port 443 proto tcp
+ufw allow from 2a06:98c0::/29 to any port 443 proto tcp
+ufw allow from 2c0f:f248::/32 to any port 443 proto tcp
+
+fi
+
+echo y | ufw enable
